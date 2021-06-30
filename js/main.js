a0f62bd917c395be486901e9e7bba15ea1d6e4e1
Tiago <andrade.tiago166@gmail.com>
2021-06-30 15:59:12 -0300

    Responsividade de 800px adicionada

--- js/main.js
+++ js/main.js
@@ -5,3 +5,108 @@ btnMenu.addEventListener('click', () =>{
 
     nav.classList.toggle("active");
 })
+
+// Main Slider
+const mainSlide = new Swiper('.main-slider', {
+  navigation: {
+    nextEl: '.btn-next',
+    prevEl: '.btn-prev',
+  },
+  });
+
+  // Bicycle Shop Slide
+  const bicycleSlide = new Swiper(".bicycle-shop-slider", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.box-slider .btn-next.bic',
+      prevEl: '.box-slider .btn-prev.bic',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+
+      850: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  // Components Shop Slide
+  const componentsSlide = new Swiper(".components-shop-slider", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.components .box-slider .btn-next.comp',
+      prevEl: '.components .box-slider .btn-prev.comp',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+
+      850: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  // Acessories Shop Slide
+  const acessoriesSlide = new Swiper(".acessories-shop-slider", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.acessories .box-slider .btn-next.aces',
+      prevEl: '.acessories .box-slider .btn-prev.aces',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+
+      850: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
+  });
+  
